413a3762c44b1639c8ae2d726840eb5181dedb01
Juan Marquez <juanmarquez4@gmail.com>
2022-07-11 22:18:27 +1000

    added uvi index color indicators

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,51 @@
 let inputBtn = $("#btn");
+let cityBtn = $(".cityBtn")
 let currentCard = $("#currentCard");
 let futureCard = $("#futureCard");
 let mesageCard = $("#mesageCard");
 let currentTitle = $("#currentTitle");
 let forecastTitle = $("#forecastTitle");
+let menu = $("#menu");
 
-// search button finction
+// search for a city button function
 inputBtn.click (function(){
-    getApi();
+    const city = $("input").val();
+    if(city!=""){
+        getApi(city);
+        searchedCity(city);
+        $("input").val("") 
+    }
+});
+
+// saved city button function
+cityBtn.on("click","button", function(){
+    const city = $(this).text();
+    getApi(city);
 })
 
-// first API call to obtain lon and lat from searched city
-function getApi(){
+// adds city to search history
+function searchedCity (e){
+    // localStorage.setItem("city", city); TODO
+    let currentSearch =
+    `
+    <button class="btn btn-secondary mt-3" type="button">${e}</button>
+    `
+    menu.append(currentSearch);
+}
 
-    let city = $("input").val();
+
+// first API call to obtain lon and lat from searched city
+function getApi(e){
+
+    // let city = $("input").val();
     let APIKey = "2ae0cb6ae56672834d0091b96e80c99d";
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + e + "&appid=" + APIKey;
 
     fetch(queryURL)
         .then(function(response){
+        if (response.status!==200){ //if city name is not recognize by API display an alert
+        alert("City name not valid. Please enter a City name")
+        }
         return response.json();
         })
         .then(function(data){
@@ -33,23 +60,30 @@ function getApi(){
             return response.json();
             })
             .then(function(data){
-            console.log(data)
+            // console.log(data)
             
             // resets current card content
             currentCard.empty()
 
             // gets current time by time zone
             let currentDate = moment().tz(data.timezone).format("M/D/YYYY");
-            console.log(currentDate);
 
             // Current Weather Conditions
-            let cityName = city;
+            let cityName = e;
             let icon = data.current.weather[0].icon;
             let temperature = data.current.temp;
             let wind = data.current.wind_speed;
             let humidity = data.current.humidity;
             let uvi = data.current.uvi;
-    
+            let uviColor = "..."
+            if (uvi < 3){
+                uviColor = "bg-success"
+            } else if(uvi >= 3 & uvi <= 5){
+                uviColor = "bg-warning"
+            } else{
+                uviColor = "bg-danger"
+            }
+            
             let currentWeather = 
                 `
                 <div class="card col-2 p-3">
@@ -59,10 +93,12 @@ function getApi(){
                 <h6 class="card-text">Temp: ${temperature} °C</h6>
                 <h6 class="card-text">Wind: ${wind} m/s</h6>
                 <h6 class="card-text">Humidity: ${humidity} %</h6>
-                <h6 class="card-text">UV Index: ${uvi}</h6>
+                <h6 class="card-text ${uviColor}">UV Index: ${uvi}</h6>
                 </div>
-                <div class="card col ms-3"></div>
                 `
+
+            // <div class="card col ms-3"></div> Future Development TODO
+
             currentCard.append(currentWeather);
 
             // resets current card content
@@ -77,7 +113,14 @@ function getApi(){
                 let humidityDaily = data.daily[i].humidity;
                 let uviDaily = data.daily[i].uvi;
                 let futureDate = moment(currentDate).add(i+1,"d").format("M/D/YYYY");
-                console.log(futureDate);
+                let uviColorDaily = "..."
+                if (uviDaily < 3){
+                    uviColorDaily = "bg-success"
+                } else if(uviDaily >= 3 & uviDaily <= 5){
+                    uviColorDaily = "bg-warning"
+                } else{
+                    uviColorDaily = "bg-danger"
+                }
     
                 let futureWeather =
                     `
@@ -88,7 +131,7 @@ function getApi(){
                     <h6 class="card-text">Temp: ${temperatureDaily} °C</h6>
                     <h6 class="card-text">Wind: ${windDaily} m/s</h6>
                     <h6 class="card-text">Humidity: ${humidityDaily} %</h6>
-                    <h6 class="card-text">UV Index: ${uviDaily}</h6>
+                    <h6 class="card-text ${uviColorDaily}">UV Index: ${uviDaily}</h6>
                     </div>
                     `
                 futureCard.append(futureWeather);
